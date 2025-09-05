cc67c5e9c492e685e5b683ccaabc3444df16a3a2
ᴊᴏʏ ʙᴏʏ <galaxygrade00@gmail.com>
2025-09-05 05:53:23 +0000

    Error: ./src/app/requirements/page.tsx:445:7 Ecmascript file had an erro

--- src/app/requirements/page.tsx
+++ src/app/requirements/page.tsx
@@ -36,7 +36,7 @@ import {
 } from '@/components/ui/card';
 import { useToast } from '@/hooks/use-toast';
 import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
-import { CalendarIcon, RemixIcon } from 'lucide-react';
+import { CalendarIcon, Wand2 as RemixIcon } from 'lucide-react';
 import { Calendar } from '@/components/ui/calendar';
 import { cn } from '@/lib/utils';
 import { format } from 'date-fns';
@@ -441,14 +441,3 @@ export default function RequirementsPage() {
     </div>
   );
 }
-
-const RemixIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-12h2v4h-2zm0 6h2v2h-2zM9.5 8.5l1.5 1.5-1.5 1.5zm4 0l1.5 1.5-1.5 1.5z" />
-  </svg>
-);
